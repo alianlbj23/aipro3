8542d30ae4ae879fd76487d7461558d05600b972
alianlbj23 <alianlbj23@gmail.com>
2022-02-15 17:01:49 +0800

    刪除副

--- mysite/views.py
+++ mysite/views.py
@@ -8,7 +8,7 @@ from .models import *
 
 def home(request):
     pictures = picture.objects.all()
-    student_width = '95%'
+    student_width = '95.7%'
     #teacher_width = '100%'
     return render(request, 'index.html', locals())
 
